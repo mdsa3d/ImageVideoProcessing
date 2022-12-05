3c0743206ff398e839ac821eccb03dc7c1638c82
Mohammad Saad <saadsiraj254@gmail.com>
2022-12-05 22:38:48 +0800

    update startapp

--- startapp.bat
+++ startapp.bat
@@ -3,5 +3,5 @@ title [Extract Image From Video - WebApp]
 set path=%CD%
 call %path%\venv\Scripts\activate.bat
 cd %path%\src
-call streamlit run src\app.py --server.port 80
+call streamlit run src\app.py --server.port 80 --server.maxUploadSize 200000
 @REM @echo %path%
